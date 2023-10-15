ab8c3d5899e5d717ed5b3b399b4fa2b1d0da99c6
name <massimoganzerli@yahoo.com>
2023-10-15 22:20:23 +0200

    Add files via upload

--- controllers.c
+++ controllers.c
@@ -1,5 +1,4 @@
-
-/* INFO ABOUT REQUEST
+/* // // // // // // // // // // // // // // // // // // // //  INFO ABOUT REQUEST
 
 struct HTTP_request{
     char* method;
@@ -10,32 +9,27 @@ struct HTTP_request{
     unsigned int attributes_count;
     char* body;
 }http_request;
-
 */
 
+// // // // // // // // // // // // // // // // // // // // // GET CONTROLLERS
 
-
-// GET CONTROLLERS
-
-// /favicon.ico
+// '/favicon.ico'
 void ctrl_favicon(){
+        add_response_header("Content-Type: image/jpeg");
         response_send_pic("image.jpeg");
 
 }
 
-// /
+// '/'
 void ctrl_home(){
-
         add_response_header("Server: E-pache 1.0");
         response_send_file("views/html.html");
 
 }
 
-
-
-
-
-// POST CONTROLLERS
+// // // // // // // // // // // // // // // // // // // // // POST CONTROLLERS
+//  '/'
 void ctrl2(){
+        add_response_header("Content-Type: image/jpeg");
         response_send_pic("image.jpeg");
 }
