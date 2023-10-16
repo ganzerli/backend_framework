1a2630e9b31232d063acbee4e7160934a33aad90
name <massimoganzerli@yahoo.com>
2023-10-17 00:30:37 +0200

    prelude of view engine

--- controllers.c
+++ controllers.c
@@ -15,6 +15,66 @@ struct HTTP_request{
 
 // // // // // // // // // // // // // // // // // // // // GET CONTROLLERS
 
+
+//#########################################################################
+//          P R E L U D E     O F     V I E W    E N G I N E              #
+//#########################################################################
+    // this function is to keep as it is.. dont modulate it
+    int find_in_str (char * text, char* from , char* to , unsigned int* indexes){
+        unsigned int i = 0;
+        char frs_c = from[0];
+        char lst_c = to[ ( str_len(to)-1 ) ];
+        char c = text[0];
+        unsigned int delicious_lasagna(char* text, unsigned int i , char* begin  , char* end) {
+            u8 bg_sz = str_len(begin);
+            u8 nd_sz = str_len(end);
+            unsigned int index_begin = 0;
+            unsigned int index_end = 0;
+            unsigned int j = 0;
+            if(str_cmp( bg_sz , begin , &text[i] ) ){
+                index_begin = i;
+                j = bg_sz;
+                while ( text[ i + j ] != '\0'){
+                    if( text [i + j] == end[0]){
+                        if (str_cmp(nd_sz , end , &text[i+j])){
+                            index_end = i + j + nd_sz - 1;
+                            return index_end;
+                        }
+                    }
+                    j++;
+                }
+            } 
+            return 0;
+        }
+        unsigned int counter = 0;
+        unsigned int index_end = 0;
+        while (c != '\0'){
+            if(c == frs_c){
+                // check if
+                index_end = delicious_lasagna( text , i , "<-" , "->");
+                if(index_end != 0){
+                    indexes[counter] = i;
+                    indexes[counter+1] = index_end;
+                    i+= index_end;
+                    index_end = 0;
+                    counter += 2;
+                    printf("i = %u \n" , i);
+                }else{
+                    break;
+                }     
+            }
+            i++;
+            c = text[i];
+        }
+        return counter;
+    }
+//#########################################################################
+//          P R E L U D E     O F     V I E W    E N G I N E     (end)    #
+//#########################################################################
+
+
+
+
 // '/favicon.ico'
 void ctrl_favicon(){
     response_send_pic("E-pachy_512x512.png");
@@ -23,83 +83,12 @@ void ctrl_favicon(){
     // '/'
 void ctrl_home(){
 
-
     unsigned int indexes[512];
-    char* sentence = "hello <-abc-> hello2 , ansdn sdlkjsd slkjslkja ,lks <-a-> dklslkjdlkjsd <-b->";
+    char* text = "hello <-abc-> hello2 ,<- ansdn <-sdlkjsd slkjslkja ,lks <-a dklslkjd->lkjsd b->"
 
+    unsigned int counter = find_in_str( text , "<-" , "->" , indexes );
 
-    void find_in_str (char * str, char* from , char* to , unsigned int* indexes){
-        
-        unsigned int i = 0;
-
-        char frs_c = from[0];
-        char lst_c = to[ ( str_len(to)-1 ) ];
-
-        char c = sentence[0];
-
-        unsigned int check_(char* sentence, unsigned int i , char* begin  , char* end) {
-
-            u8 bg_sz = str_len(begin);
-            u8 nd_sz = str_len(end);
-
-            unsigned int index_begin = 0;
-            unsigned int index_end = 0;
-
-            unsigned int j = 0;
-
-            if(str_cmp( bg_sz , begin , &sentence[i] ) ){
-                index_begin = i;
-                j = bg_sz;
-
-                while ( sentence[ i + j ] != '\0'){
-
-                    if( sentence [i + j] == end[0]){
-                        printf("found at %u\n" , i+j );
-
-                        if ( str_cmp(nd_sz , end , &sentence [ i + j ] ) ){
-
-                            index_end = i + j + nd_sz - 1;
-                            return index_end;
-
-                        }
-                    }
-                    j++;
-                }
-            } 
-
-            printf("begin: %u ,  end %u" , index_begin , index_end);
-            return 0;
-        }
-        unsigned int counter = 0;
-        unsigned int index_end = 0;
-        while (c != '\0'){
-            printf("%c" , c);
-
-            if(c == frs_c){
-                // check if
-                index_end = check_( sentence , i , "<-" , "->");
-                if(index_end != 0){
-                    indexes[counter] = i;
-                    indexes[counter+1] = index_end;
-                    index_end = 0;
-                    counter += 2;
-                }else{
-                    break;
-                }     
-
-            }
-            i++;
-            c = sentence[i];
-        }
-
-        for(i = 0 ; i < counter; i+=2 ) printf("begin -> [%u] , end -> [%u]" , indexes[i] , indexes [i+1]);
-
-    }
-    
-    
-    find_in_str(sentence , "<-" , "->" , indexes);
-
-
+    for (u8 i = 0 ; i < counter; i++ ) printf("[%u]" , indexes[i] );
 
     add_response_header("Server: E-pache 1.0");
     response_send_file("views/html.html");
